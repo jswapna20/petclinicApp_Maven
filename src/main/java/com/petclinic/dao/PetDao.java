6a6050da3080d44867ee756ddc1c1f73c9e2debc
Swapna <Swapna@LAPTOP-B2L6EL3A>
2020-07-17 23:31:52 +0530

    second commit

--- src/main/java/com/petclinic/dao/PetDao.java
+++ src/main/java/com/petclinic/dao/PetDao.java
@@ -14,7 +14,7 @@ import com.petclinic.entity.PetOwner;
 import com.petclinic.entity.Visit;
 
    @Repository
-   public class PetDao extends BaseDao{
+   public class PetDao extends BaseDao{ 
 		 /*private static SessionFactory sessionFactory;
 
 		 public static void main(String[] args){
